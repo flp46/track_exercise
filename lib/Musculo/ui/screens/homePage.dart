81c170cb035668409c2ef9925bc7e78df1ac367d
flp46 <alarconnieto@gmail.com>
2020-01-21 21:36:52 -0500

    agregando lfoto de perfil, texto y peso

--- lib/Musculo/ui/screens/homePage.dart
+++ lib/Musculo/ui/screens/homePage.dart
@@ -3,44 +3,78 @@ import 'package:flutter/widgets.dart';
 
 class HeaderAppInfo extends StatelessWidget{
 
-  String motivationText = 'Rise and Grind';
+  String motivationText = 'Rise and Grind today es your day';
   Image profilePicture;
-  int weight;
+  String weight = '67';
 
+
+//Texto motivacional del header
   Widget getMotivationText(){
     return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.blue
-      ),
+      margin: EdgeInsets.only(left: 17),
+      height: 20,
+      width: 250,
       child: Text(
         motivationText
       ),
     );
   }
 
+
+//Imagen de perfil del usuario
   Widget profileIMage(){
     return Container(
-      width: 20,
-      height: 20,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage('assets/shared/profile_pictur.jpeg'),
-          fit: BoxFit.cover
-        )
-      ),
-    );
+        margin: EdgeInsets.only(left: 15),
+        width: 80,
+        height: 90,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/shared/profile_picture.jpeg'),
+            fit: BoxFit.cover,
+          ),
+          shape: BoxShape.circle
+        ),
+      );
   }
+
+
+  Widget actualWeight(){
+    return Container(
+      margin: EdgeInsets.only(left: 17),
+      height: 20,
+      width: 40,
+      child: Text(
+        weight 
+      )
+    );
+  } 
+  
+
+
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Stack(
-      children: <Widget>[
-        profileIMage(),
-        getMotivationText()
-      ],
+    return Container(
+
+      width: double.infinity,
+      height: 200,
+      child: Row( 
+        children: <Widget>[
+          Container(
+            child: profileIMage()
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              getMotivationText(),
+              actualWeight()
+            ],
+          ),
+        ],
+      ),
     );
     
   }
